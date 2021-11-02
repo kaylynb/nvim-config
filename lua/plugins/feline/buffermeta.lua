04faff1538ca8f7824bb2825d6a734613d41fbef
Kaylyn Bogle <kaylyn@kaylyn.ink>
2021-11-01 23:14:18 -0700

    Update feline with new api

--- lua/plugins/feline/buffermeta.lua
+++ lua/plugins/feline/buffermeta.lua
@@ -1,14 +1,15 @@
 local M = {}
 
-M.line_percentage = function(winid)
+M.line_percentage = function()
+	local winid = vim.api.nvim_get_current_win()
 	local current_line = vim.api.nvim_win_get_cursor(winid)[1]
 	local lines = vim.api.nvim_buf_line_count(vim.api.nvim_win_get_buf(winid))
 
 	return string.format('%3d%%%%', math.floor(current_line / lines * 100))
 end
 
-M.position = function(winid)
-	local row, col = unpack(vim.api.nvim_win_get_cursor(winid))
+M.position = function()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win()))
 	return string.format('%3d:%-2d', row, col + 1)
 end
 
--- lua/plugins/feline/filemeta.lua
+++ lua/plugins/feline/filemeta.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-M.file_encoding = function(winid)
-	local bufnr = vim.api.nvim_win_get_buf(winid)
+M.file_encoding = function()
+	local bufnr = vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win())
 
 	return (vim.bo[bufnr].fenc ~= '' and vim.bo[bufnr].fenc) or vim.o.enc
 end
 
-M.file_type = function(winid)
-	return vim.bo[vim.api.nvim_win_get_buf(winid)].filetype
+M.file_type = function()
+	return vim.bo[vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win())].filetype
 end
 
-M.file_format = function(winid)
-	return vim.bo[vim.api.nvim_win_get_buf(winid)].fileformat
+M.file_format = function()
+	return vim.bo[vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win())].fileformat
 end
 
 return M
